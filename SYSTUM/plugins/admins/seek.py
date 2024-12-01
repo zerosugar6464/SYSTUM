5a9464f973016af0a40c0cc66c7ce448f265f2d3
Silahanim21 <turkcell63632@gmail.com>
2024-12-01 09:48:14 +0300

    Update seek.py

--- SYSTUM/plugins/admins/seek.py
+++ SYSTUM/plugins/admins/seek.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["seek", "cseek", "seekback", "cseekback"])
+    filters.command(["ilerisar", "cseek", "gerisar", "cseekback"])
     & filters.group
     & ~BANNED_USERS
 )
