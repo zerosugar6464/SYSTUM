b1bf44dc5ebe0115acda27c84c5326e79ae1a49e
Silahanim21 <turkcell63632@gmail.com>
2024-12-01 09:48:57 +0300

    Update loop.py

--- SYSTUM/plugins/admins/loop.py
+++ SYSTUM/plugins/admins/loop.py
@@ -8,7 +8,7 @@ from SYSTUM.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["loop", "cloop"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["tekrarla", "cloop"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_17"]
