5946cfaba3cf33b70bc6cc3de671c768e166472b
Silahanim21 <turkcell63632@gmail.com>
2024-12-01 09:47:48 +0300

    Update resume.py

--- SYSTUM/plugins/admins/resume.py
+++ SYSTUM/plugins/admins/resume.py
@@ -9,7 +9,7 @@ from SYSTUM.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["dewam", "cresume"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
