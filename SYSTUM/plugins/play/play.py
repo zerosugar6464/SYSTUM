67d6d2598f8beb3e6a996212cb95e8cc92232a52
Silahanim21 <turkcell63632@gmail.com>
2024-12-01 09:46:46 +0300

    Update play.py

--- SYSTUM/plugins/play/play.py
+++ SYSTUM/plugins/play/play.py
@@ -28,8 +28,8 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "play",
-            "vplay",
+            "oynat",
+            "voynat",
             "cplay",
             "cvplay",
             "playforce",
